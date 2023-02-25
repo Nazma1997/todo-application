722c3178fd729228592a1f8913dd38933149d269
Nazma1997 <nazma.ist.707@gmail.com>
2023-02-25 14:27:20 +0600

    add todo reducer

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+  {
+    id: 1,
+    text: 'Todo One',
+    completed: true,
+  },
+  {
+    id: 2,
+    text: 'Todo Two',
+    completed: false,
+    color: 'red'
+  }
+]
